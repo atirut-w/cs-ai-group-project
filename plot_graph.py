1d4599dca9dee0d83faa58f5cb5defa0340c1805
WarinCode <warinsaipanya471@outlook.co.th>
2025-02-05 20:44:04 +0700

    Added comments

--- plot_graph.py
+++ plot_graph.py
@@ -7,6 +7,7 @@ from models.PolynomialRegression import PolynomialRegressionModel
 from models.DecisionTreeRegressorModel import DecisionTreeRegressionModel
 from utils.typing.type import Float
 
+# นำเข้า models ทั้งหมดแต่ละ  module เก็บไว้ใน tuple
 models: Tuple[Module, ...] = (
     LinearRegressionModel(),
     MultipleRegessionModel(),
@@ -14,17 +15,23 @@ models: Tuple[Module, ...] = (
     DecisionTreeRegressionModel()
 )
 
+# list สำหรับเก็บค่า accuracy กับค่า error
 acc_vals: List[Float] = []
 err_vals: List[Float] = []
 
+# วน loop รับค่า element ที่เป็น model ของแต่ล่ะตัว
 for model in models:
+    # เรียกใช้ method ของ model โดยให้ทำตามขั้นตอน
     model.prepare_dataset()
     model.train_model()
     model.prediction()    
     model.evaluate_model()
+    # เก็บค่า accuracy และ error ของแต่ล่ะ model
     acc_vals.append(model.get_accuracy_value())
     err_vals.append(model.get_error_value())
     
+# สร้าง instance ของ GraphMaker โดยส่งค่า args ไปด้วย
 maker: GraphMaker = GraphMaker(acc_vals, err_vals)
+# เรียกใช้ methods สร้าง graph
 maker.create_accuracy_graph()
 maker.create_error_graph()
--- utils/Helper.py
+++ utils/Helper.py
@@ -1,11 +1,14 @@
 from utils.typing.type import Float
 import math
 
+# class Helper จะมี utility methods สำหรับนำไปใช้ในส่วนอื่นๆของ modules
 class Helper:
+    # methods สำหรับการแปลงเลขทศนิยมเป็นค่าเปอร์เซ็น
     @staticmethod
     def convert_to_100_percent(n: Float) -> int:
         return math.trunc(n * 100)
     
+    # methods สำหรับการส่ง path ไฟล์ dataset ของข้อมูล
     @staticmethod
     def get_data_path() -> str:
         return "datasets/data.csv"
